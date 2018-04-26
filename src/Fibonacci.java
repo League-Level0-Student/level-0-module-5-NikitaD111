e966f3c854e82961cc3a8e99959c3690a4d7c5a3
isabelledl <league@WorthyMini-5.attlocal.net>
2018-04-25 18:23:57 -0700

    Fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,4 +1,16 @@
 
 public class Fibonacci {
-
+	public static void main(String[] args) {
+		int f = 0;
+		int s = 1;
+		for (int i = 0; i < 12; i++) {
+			int c = f + s;
+			if (i % 2 == 0) {
+				f = c;
+			} else {
+				s = c;
+			}
+			System.out.println(c);
+		}
+	}
 }
